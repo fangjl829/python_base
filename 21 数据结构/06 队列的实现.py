be5c3e0d86c7f153323d0fd522e5daaa3dafbf9f
ciossfang <fangjl829@163.com>
2021-08-31 09:00:25 +0800

    Default Changelist

---
+++ 21 数据结构/06 队列的实现.py
@@ -0,0 +1,28 @@
+
+# 脚本用途：自动化运维
+# 编写者：方胜山（ciossfang）
+# 编写时间：2021/8/31 08:57
+# !/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+#导入模块
+from timeit import Timer
+
+#定义append_test
+def append_test():
+    li=[]
+    for i in range(10000):
+        li.append(i)
+
+def insert_test():
+    li=[]
+    for i in range(10000):
+        li.insert(0,i)
+
+#测试执行时间
+append_timer=Timer('append_test()','from __main__ import append_test')
+print('append插入执行时间：',append_timer.timeit(1000))
+
+insert_timer=Timer('insert_test()','from __main__ import insert_test')
+print('insert插入执行时间：',insert_timer.timeit(1000))
